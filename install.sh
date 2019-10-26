27671e64cba985760584f4ebd3ed75ee05e088bc
b1izzard-34 <sanketkumbhar9634@gmail.com>
2019-10-26 18:57:20 +0530

    install script to download wallpapers

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-for i in "$@"
-do
-		echo "Adding $1 to wallpapers ..."
-		cd $i
-		sudo mkdir /usr/share/backgrounds/gnome/$1-timed
-		sudo cp $i*.jpeg /usr/share/backgrounds/gnome/$1-timed
-		sudo cp $i-timed.xml /usr/share/backgrounds/gnome
-		sudo cp $i.xml /usr/share/gnome-background-properties
-		echo "Added $1 dynamic wallpapers!"
-done
+echo "Adding $1 to wallpapers ..."
+echo "Starting download ..."
+
+svn checkout https://github.com/b1izzard-34/Dynamic-gnome-wallpapers/trunk/$1
+
+cd $1
+sudo mkdir /usr/share/backgrounds/gnome/$1-timed
+sudo cp $1*.jpeg /usr/share/backgrounds/gnome/$1-timed
+sudo cp $1-timed.xml /usr/share/backgrounds/gnome
+sudo cp $1.xml /usr/share/gnome-background-properties
+echo "Added $1 dynamic wallpaper!"
+
+echo "Cleaning up ..."
+rm -rf $1
